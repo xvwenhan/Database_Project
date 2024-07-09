0ec83865da22560ce04d24b7a95529986a8c590e
xvwenhan <xvwenhan@outlook.com>
2024-07-09 20:24:53 +0800

    添加了一些模型类

---
+++ BackendCode/BackendCode/Models/MARKET_STORE.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackendCode.Models
+{
+    public class MARKET_STORE
+    {
+        [Key]
+        [MaxLength(100)]
+        [Column("MARKET_ID")]
+        public string MARKET_ID { get; set; }
+
+        [MaxLength(100)]
+        [Column("STORE_ACCOUNT_ID")]
+        public string STORE_ACCOUNT_ID { get; set; }
+
+
+        [Column("IN_OR_NOT ")]
+        public bool IN_OR_NOT { get; set; }
+    }
+}
---
+++ BackendCode/BackendCode/Models/PRODUCT.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BackendCode.Models
+{
+    public class PRODUCT
+    {
+        [Key]
+        [Column("PRODUCT_ID")]
+        [Required]
+        [MaxLength(100)]
+        public string PRODUCT_ID { get; set; }
+
+        [Required]
+        [Column("PRODUCT_NAME")]
+        [MaxLength(50)]
+        public string PRODUCT_NAME { get; set; }
+
+        [Required]
+        [Column("PRODUCT_PRICE")]
+        [Range(0, 99999.99)]
+        public decimal PRODUCT_PRICE { get; set; }
+
+        [Required]
+        [Column("SALE_OR_NOT")]
+        public bool SALE_OR_NOT { get; set; }
+
+        [Required]
+        [Column("TAG")]
+        [MaxLength(50)]
+        public string TAG { get; set; }
+
+        [Required]
+        [Column("DESCRIBTION")]
+        [MaxLength(200)]
+        public string DESCRIBTION { get; set; }
+
+        [Required]
+        [Column("ACCOUNT_ID")]
+        [MaxLength(100)]
+        public string ACCOUNT_ID { get; set; }
+
+        // 导航属性
+        [ForeignKey("ACCOUNT_ID")]
+        public STORE Store { get; set; }
+    }
+}
