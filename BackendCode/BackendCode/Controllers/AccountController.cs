dc3cdf9d3eb549513fa7736643b8fedb0a8e4bcb
POTUS47 <panly@tongji.edu.cn>
2024-07-11 11:52:01 +0800

    找回了失去的注册

--- BackendCode/BackendCode/Controllers/AccountController.cs
+++ BackendCode/BackendCode/Controllers/AccountController.cs
@@ -24,6 +24,36 @@ namespace Account.Controllers
 
         //注册
         //前端自行完成用户名等为空的错误判断
+        [HttpGet("/register")]
+        public IActionResult UserRegister(string u_name, string p_number, string passwd, string u_gender, int u_age, string u_address)
+        {
+            var tempAccount = _dbContext.BUYERS.FirstOrDefault(a => a.PHONE_NUMBER == p_number);
+            if (tempAccount != null)//如果该手机号存在
+            {
+                return BadRequest($"该手机号已经被注册");//返回账号已存在
+            }
+            else//如果是新手机号
+            {
+                Random random = new();
+                int _userId = random.Next(1, 10000000);
+                string a = _userId.ToString();
+                string uidb = "U" + a;
+
+                _dbContext.BUYERS.Add(new BackendCode.Models.BUYER()
+                {
+                    ACCOUNT_ID = uidb,
+                    USER_NAME = u_name,
+                    PHONE_NUMBER = p_number,
+                    PASSWORD = passwd,
+                    ADDRESS = u_address,
+                    GENDER = u_gender,
+                    AGE = u_age,
+                    TOTAL_CREDITS = 0
+                });
+                _dbContext.SaveChanges();
+                return Ok(uidb);
+            }
+        }
 
 
         [HttpGet("/login")]
