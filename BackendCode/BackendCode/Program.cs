49a68ee587548ef7d620ca56f5dbea033e6e1544
xvwenhan <xvwenhan@outlook.com>
2024-07-12 13:59:45 +0800

    基本完成账号功能
    包含账号的注册登录登出找回密码发送邮件功能。添加了cookie，密码的哈希加密。此版本为了方便测试，给部分正式代码进行了注释。跨域cor部分代码勿动，丞待解决前后端联调时接口访问被拒绝的问题。

--- BackendCode/BackendCode/Program.cs
+++ BackendCode/BackendCode/Program.cs
@@ -1,10 +1,25 @@
 using Microsoft.EntityFrameworkCore;
 using BackendCode.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+//使用cookie
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/api/account/login"; // 设置登录路径
+        options.AccessDeniedPath = "/api/account/accessdenied"; // 设置拒绝访问路径
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(60); // 设置Cookie过期时间
+
+        options.Cookie.HttpOnly = true;
+        options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+        options.Cookie.SameSite = SameSiteMode.None; // 允许跨域 Cookie
+    });
 
 //解决跨域问题
 builder.Services.AddCors(options =>
@@ -15,8 +30,20 @@ builder.Services.AddCors(options =>
                    builder.AllowAnyOrigin()
                        .AllowAnyMethod()
                        .AllowAnyHeader();
+
+                   /*     builder.AllowCredentials(); // 允许携带凭据。需要下一个microsoft.aspnetcore.cor包才能用.
+                                                    // 但它为什么和AllowAnyOrigin()只能用一个？？？*/
                });
+
+    /*    options.AddPolicy("AllowSpecificOrigin",
+               builder => builder
+                   .WithOrigins("http://localhost:8081") // 允许的前端地址   mmy:http://100.79.96.89:8080
+                   .AllowAnyHeader()
+                   .AllowAnyMethod()
+                   .AllowCredentials());*/
 });
+
+
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -30,16 +57,19 @@ builder.Services.AddControllers();
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    //app.UseDeveloperExceptionPage();//新加的 不知道干啥的
     app.UseSwagger();
     app.UseSwaggerUI();
     app.UseCors("AllowAnyOrigin");
+    //app.UseCors("AllowSpecificOrigin");// 使用 CORS 中间件
 }
 
 app.UseHttpsRedirection();
-
+//app.UseAuthentication(); // 必须在 UseAuthorization 之前调用.用于正确配置身份验证中间件
 app.UseAuthorization();
 
 app.MapControllers();
