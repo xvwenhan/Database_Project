c3afe3f9125311ac2025b7acc709e526e972cfe4
newoneinhere <152025278+newoneinhere@users.noreply.github.com>
2024-07-12 19:10:20 +0800

    Add files via upload
    做了商家的框架和四个页面

---
+++ github第一次提交/页面代码/views/BusinessCommodity.vue
@@ -0,0 +1,33 @@
+<template>
+   <div class="BusinessCommodity">
+        <BusinessCommodityTopbar @changeView="handleChangeView" />
+        <div class="CommodityContent">
+        <component :is="currentView"></component>
+        </div> 
+   </div>
+</template>
+
+<script>
+import BusinessCommodityShow from './BusinessCommodityShow.vue'
+import BusinessCommodityTopbar from './BusinessCommodityTopbar.vue'
+
+export default {
+    name:'BusinessCommodity',
+    components: {
+    BusinessCommodityShow,
+    BusinessCommodityTopbar,
+  },
+  data() {
+    return {
+      currentView: 'BusinessCommodityShow'
+    }
+  },
+  methods: {
+    handleChangeView(view) {
+      this.currentView = view;
+    }
+  }
+};
+</script>
+
+
---
+++ github第一次提交/页面代码/views/BusinessOrderManage.vue
@@ -0,0 +1,32 @@
+  <template>
+    <div class="BusinessOrder">
+         <BusinessOrderTopbar @changeView="handleChangeView" />
+         <div class="CommodityContent">
+         <component :is="currentView"></component>
+         </div> 
+    </div>
+ </template>
+ 
+ <script>
+ import BusinessOrderShow from './BusinessOrderShow.vue'
+ import BusinessOrderTopbar from './BusinessOrderTopbar.vue'
+ 
+ export default {
+     name:'BusinessOrder',
+     components: {
+     BusinessOrderShow,
+     BusinessOrderTopbar,
+   },
+   data() {
+     return {
+       currentView: 'BusinessOrderShow'
+     }
+   },
+   methods: {
+     handleChangeView(view) {
+       this.currentView = view;
+     }
+   }
+ };
+ </script>
+ 
