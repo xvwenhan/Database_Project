d484d51d5f7360f97896c47109dcb338c01f5fab
ryl1ie <1239716933@qq.com>
2024-07-30 20:00:06 +0800

    添加商家视角的市集页面接口

---
+++ BackendCode/BackendCode/Controllers/StoreOrderController.cs
@@ -0,0 +1,28 @@
+﻿using BackendCode.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using BackendCode.DTOs.Store;
+using StoreViewMarket.Controllers;
+
+namespace StoreOrderController.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StoreOrderController : ControllerBase
+    {
+        private readonly YourDbContext _dbContext;
+        private readonly ILogger<StoreViewMarketController> _logger;
+
+        public StoreOrderController(YourDbContext dbContext, ILogger<StoreViewMarketController> logger)
+        {
+            _dbContext = dbContext;
+            _logger = logger;
+        }
+   
+
+    }
+}
---
+++ BackendCode/BackendCode/DTOs/StoreDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace BackendCode.DTOs.Store
+{
+    public class MarketDTO
+    {
+        public string MarketId { get; set; }
+        public string? Theme { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public string? Detail { get; set; }
+        public string? PosterImg { get; set; } // URL or Base64 string of the image
+        public bool IsStoreParticipating { get; set; }
+    }
+
+    public class UpdateMarketStoreDTO
+    {
+        public string MarketId { get; set; }
+        public string StoreId { get; set; }
+        public bool InOrOut { get; set; }
+    }
+}
