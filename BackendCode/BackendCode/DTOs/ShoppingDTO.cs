3de6b865dcd54626b3346f776c3c9ca204cb239a
ttttttttttttx <wwn@tongji.edu.cn>
2024-08-02 17:24:50 +0800

    买家购物相关接口

---
+++ BackendCode/BackendCode/DTOs/ShoppingDTO.cs
@@ -0,0 +1,31 @@
+﻿namespace BackendCode.DTOs.Shopping
+{
+    public class StoreInfoDTO
+    {
+        public string name { get; set; }
+        public decimal? score { get; set; }
+    }
+
+    public class AuthenticationInfoDTO
+    {
+        public string storeID { get; set; }
+        public List<IFormFile>? image { get; set; }
+        public string description { get; set; }
+    }
+
+    public class SetStoreInfoDTO
+    {
+        public string storeID { get; set; }
+        public string address { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+    }
+
+    public class GetStoreInfoDTO
+    {
+        public string storeID { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+    }
+}
