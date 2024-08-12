75ee6d7f4ff10562ad0675b2a4cb03b81eda5330
Joye <2322009829@qq.com>
2024-08-12 16:57:24 +0800

    修改个人中心弹窗

--- src/views/UserCenter.vue
+++ src/views/UserCenter.vue
@@ -427,9 +427,9 @@ async resetPassword() {
 </script>
 
 <style scoped>
-.modal {
+  .modal {
     display: flex;
-    align-items: center;
+    /* align-items: center; */
     justify-content: center;
     position: fixed;
     z-index: 1000;
@@ -442,6 +442,8 @@ async resetPassword() {
   }
   
   .modal-content {
+    margin-top: 200px;
+    height: 45%; /*控制卡片的高度*/
     background-color: #fefefe;
     padding: 20px;
     border-radius: 8px;
@@ -467,6 +469,7 @@ async resetPassword() {
 }
 
 .user-center {
+  /* height: 50%; */
   position: relative;
   padding: 20px;
   background-color: #fff;
