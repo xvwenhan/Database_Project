a2b74b51dec7237de55289ab3c9aa5230c6c87bd
POTUS47 <111119326+POTUS47@users.noreply.github.com>
2024-08-13 11:28:22 +0800

    商家也可以更改头像和简介

--- BackendCode/BackendCode/Controllers/UserInfoController.cs
+++ BackendCode/BackendCode/Controllers/UserInfoController.cs
@@ -48,71 +48,114 @@ namespace UserInfo.Controllers
         [HttpPost("GetPhotoAndDescribtion")]
         public async Task<IActionResult> GetPhotoAndDescribtion([FromBody]GPADModel model)
         {
-            var infos = await _dbContext.BUYERS
+            string type = model.Id.Substring(0, 1);
+            if (type == "U")
+            {
+                var infos = await _dbContext.BUYERS
                 .Where(b => b.ACCOUNT_ID == model.Id)
                 .SingleOrDefaultAsync();
+                if (infos == null)
+                {
+                    return NotFound("不存在该用户");
+                }
 
-            if (infos == null)
-            {
-                return NotFound("不存在该用户");
+                var response = new PhotoAndDescribtionDTO
+                {
+                    Photo = infos.PHOTO != null ? Convert.ToBase64String(infos.PHOTO) : null,
+                    Describtion = infos.DESCRIBTION,
+                };
+                return Ok(response);
             }
-
-            var response = new PhotoAndDescribtionDTO
+            else if(type == "S")
             {
-                Photo= infos.PHOTO != null ? Convert.ToBase64String(infos.PHOTO) : null,
-                Describtion =infos.DESCRIBTION,
-            };
-            return Ok(response);
+                var infos = await _dbContext.STORES
+                .Where(b => b.ACCOUNT_ID == model.Id)
+                .SingleOrDefaultAsync();
+                if (infos == null)
+                {
+                    return NotFound("不存在该用户");
+                }
+
+                var response = new PhotoAndDescribtionDTO
+                {
+                    Photo = infos.PHOTO != null ? Convert.ToBase64String(infos.PHOTO) : null,
+                    Describtion = infos.DESCRIBTION,
+                };
+                return Ok(response);
+            }
+            else
+            {
+                return BadRequest("错误的账号类型：请检查Id是否为买家或商家");
+            }
+            
+
+            
         }
 
         // 修改头像或简介
         [HttpPut("SetPhotoAndDescribtion")]
         public async Task<IActionResult> SetPhotoAndDescribtion([FromBody] SPADModel model)
         {
-            string des = "";
-            string pho = "";
-            // 查找用户
-            /*var temp = await _dbContext.ACCOUNTS
-                .FirstOrDefaultAsync(a => a.ACCOUNT_ID == model.Id);*/
-            var temp = await _dbContext.BUYERS
+            string type = model.Id.Substring(0, 1);
+            string res = "";
+            if (type == "U")
+            {
+                var temp = await _dbContext.BUYERS
                 .Where(b => b.ACCOUNT_ID == model.Id)
                 .SingleOrDefaultAsync();
 
-            if (temp == null)
-            {
-                return NotFound("不存在该用户");
-            }
+                if (temp == null)
+                {
+                    return NotFound("不存在该用户");
+                }
 
-            if (model.Describtion !=null)
+                if (model.Describtion != null)
+                {
+                    temp.DESCRIBTION = model.Describtion;
+                    res += "简介已被成功更改 ";
+                }
+
+                if (model.Photo != null)
+                {
+                    byte[] photoBytes = Convert.FromBase64String(model.Photo);
+                    temp.PHOTO = photoBytes;
+                    res += "头像已被成功更改 ";
+                }
+            }
+            else if(type == "S")
             {
-                temp.DESCRIBTION = model.Describtion;
-                des = "简介已被成功更改";
+                var temp = await _dbContext.STORES
+                .Where(b => b.ACCOUNT_ID == model.Id)
+                .SingleOrDefaultAsync();
+
+                if (temp == null)
+                {
+                    return NotFound("不存在该用户");
+                }
+
+                if (model.Describtion != null)
+                {
+                    temp.DESCRIBTION = model.Describtion;
+                    res += "简介已被成功更改 ";
+                }
+
+                if (model.Photo != null)
+                {
+                    byte[] photoBytes = Convert.FromBase64String(model.Photo);
+                    temp.PHOTO = photoBytes;
+                    res += "头像已被成功更改 ";
+                }
             }
             else
             {
-                des = "简介未被更改";
+                return BadRequest("错误的账号类型：请检查Id是否为买家或商家");
             }
-
-            if (model.Photo != null)
-            {
-                /*var ms = new MemoryStream();
-                var image = model.Photo;
-                await image.CopyToAsync(ms);
-                var imageData = ms.ToArray();*/
-                byte[] photoBytes = Convert.FromBase64String(model.Photo);
-                temp.PHOTO = photoBytes;
-                pho = "头像已被更改";
-            }
-            else
-            {
-                pho = "头像未被更改";
-            }
-
+            
             // 保存更改
             try
             {
                 await _dbContext.SaveChangesAsync();
-                return Ok($"申请已被处理，处理结果:{des}，{pho}");
+                return Ok($"申请已被处理，处理结果:{res}");
             }
             catch (DbUpdateException ex)
             {
