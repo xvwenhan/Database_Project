4ecfd1a201565cccd9e2a2d70b80b80792b21ce0
POTUS47 <111119326+POTUS47@users.noreply.github.com>
2024-08-18 16:35:14 +0800

    更新添加商品api
    详细信息我没办法测试啊

--- BackendCode/BackendCode/DTOs/ProductDTO.cs
+++ BackendCode/BackendCode/DTOs/ProductDTO.cs
@@ -11,6 +11,13 @@
         public string? StoreTag { get; set; }
         public string? ProductPic { get; set; }  // Base64 encoded string or null
     }
+
+    public class PicDes
+    {
+        public string? Description { get; set; }
+        public string DetailPic { get; set; }
+        
+    }
     public class Product1DTO
     {
         public string? ProductName { get; set; }
@@ -18,7 +25,8 @@
         public string? Tag { get; set; }
         public string? Description { get; set; }
         public string? StoreTag { get; set; }
-        public string? ProductPic { get; set; }  // Base64 encoded string or null
+        public List<string>? ProductImages { get; set; }
+        public List<PicDes>? PicDes { get; set; }
     }
     public class Product2DTO
     {
