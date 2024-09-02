428c8913fa12c8b0bccaaaa1b7c0b77f0a5087b2
POTUS47 <111119326+POTUS47@users.noreply.github.com>
2024-09-02 15:44:46 +0800

    修改获取审核图片的接口

--- BackendCode/BackendCode/Controllers/ImageController.cs
+++ BackendCode/BackendCode/Controllers/ImageController.cs
@@ -76,7 +76,7 @@ namespace BackendCode.Controllers
             var image = _context.SUBMIT_AUTHENTICATIONS.FirstOrDefault(pd => pd.STORE_ACCOUNT_ID == imageId);
             if (image != null)
             {
-                return File(image.STORE_ACCOUNT_ID, "image/jpeg"); //根据图片类型调整MIME类型
+                return File(image.PHOTO, "image/jpeg"); //根据图片类型调整MIME类型
             }
 
             return NotFound();
