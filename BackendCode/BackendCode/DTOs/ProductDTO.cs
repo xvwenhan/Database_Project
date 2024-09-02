41b589530923c98076140726ac8f12aeb053b87d
xvwenhan <xvwenhan@outlook.com>
2024-09-02 23:31:54 +0800

    修改商品返回接口
    返回二级分类的值

--- BackendCode/BackendCode/DTOs/ProductDTO.cs
+++ BackendCode/BackendCode/DTOs/ProductDTO.cs
@@ -28,6 +28,7 @@
         public decimal ProductPrice { get; set; }
         public bool SaleOrNot { get; set; }
         public string? Tag { get; set; }
+        public string? SubTag { get; set; }//新增加
         public string? Description { get; set; }
         public string? StoreTag { get; set; }
         //public string? ProductPic { get; set; }  // Base64 encoded string or null
@@ -56,7 +57,8 @@
         public string ProductId { get; set; }
         public string? ProductName { get; set; }
         public decimal ProductPrice { get; set; }
-        public string? Tag { get; set; }
+        public string? Tag { get; set; }//分类名称
+        public string? SubTagId { get; set; }//子分类ID
         public string? Description { get; set; }
         public string? StoreTag { get; set; }
         //public string? ProductPic { get; set; }  // Base64 encoded string or null
